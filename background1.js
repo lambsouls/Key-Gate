a8d4b26dd22b92e961f3203edb980fb8d8ed5590
lambsouls <70055403+lambsouls@users.noreply.github.com>
2021-08-25 09:06:08 +0800

    Add files via upload

---
+++ background1.js
@@ -0,0 +1,21 @@
+console.log('MIT License')
+console.log('')
+console.log('Copyright (c) 2021 lambsouls');
+console.log('');
+console.log('Permission is hereby granted, free of charge, to any person obtaining a copy');
+console.log('of this software and associated documentation files (the "Software"), to deal');
+console.log('in the Software without restriction, including without limitation the rights');
+console.log('to use, copy, modify, merge, publish, distribute, sublicense, and/or sell');
+console.log('copies of the Software, and to permit persons to whom the Software is');
+console.log('furnished to do so, subject to the following conditions:');
+console.log('');
+console.log('The above copyright notice and this permission notice shall be included in all');
+console.log('copies or substantial portions of the Software.');
+console.log('');
+console.log('THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR');
+console.log('IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,');
+console.log('FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE');
+console.log('AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER');
+console.log('LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,');
+console.log('OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE');
+console.log('SOFTWARE.');
